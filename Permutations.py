3677413f037bc9d020a803b96a9ebb5c00928e8a
محمدرضا خدامی <m.r.kh.d.1381@gmail.com>
2020-09-17 12:27:44 +0430

    extract check_place method in Permutations algorithm

--- Permutations.py
+++ Permutations.py
@@ -7,6 +7,9 @@ class NQueensP(Solution):
     def solve(self):
         cols = range(self.size)
         for combo in permutations(cols, self.size):
-            if self.size == len(set(combo[i] + i for i in cols)) == len(set(combo[i] - i for i in cols)):
+            if self.check_place(combo):
                 self.solutions += 1
                 self.log(combo)
+
+    def check_place(self, positions):
+        return self.size == len(set(positions[i] + i for i in range(self.size))) == len(set(positions[i] - i for i in range(self.size)))
